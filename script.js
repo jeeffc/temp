956d6041adc574033e5935beb5f599fb650825eb
Your Name <jeeffst@gmail.com>
2023-09-12 14:46:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const key = "64479b627ebc607319574a76b165ce0b"
+
+
+function colocarDados (dados){
+    document.querySelector(".info-cidade").innerHTML= "tempo em " + dados.name
+    document.querySelector(".temp").innerHTML= Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsão").innerHTML= dados.weather[0].description
+    document.querySelector(".umidade").innerHTML= "umidade " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png` 
+
+}
+
+async function buscarCidade (cidade){
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+
+    colocarDados(dados)
+}
+
+function clickButton (){
+    const cidade = document.querySelector(".cid").value
+
+    buscarCidade(cidade)
+}
+
